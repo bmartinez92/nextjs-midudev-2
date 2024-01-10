be83a921f640576bc294ed5216e045307c8b7ba2
Brian Valdes <brianvaldesmartinez92@gmail.com>
2024-01-09 20:48:49 -0400

    Inicial-Commit

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,19 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="flex items-center justify-center py-10">
+          Hecho por la gente de Vercel
+        </footer>
+      </body>
+    </html>
+  );
+}
